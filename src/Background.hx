3d82e7214c37cfe13f351d48ec1fc296b024752a
toa5 <toa5@nau.edu>
2015-02-26 19:07:14 -0700

    Made background show up
    Parallax is all wrong though.

--- src/Background.hx
+++ src/Background.hx
@@ -14,7 +14,7 @@ class Background extends Sprite
 	var imageHeight: Float;
 	var image1: Image;
 	var image2: Image;
-	
+
 	public function new()
 	{
 		super();
@@ -24,16 +24,18 @@ class Background extends Sprite
 	public function onAddedToStage(event:Event):Void {
 
 		this.removeEventListener(Event.ADDED_TO_STAGE, onAddedToStage);
-		
+
 		image1 = new Image(Root.assets.getTexture("bg"));
 		image2 = new Image(Root.assets.getTexture("bg"));
+		addChild(image1);
+		addChild(image2);
 
 		image1.y = 0;
 		image2.y = image1.height;
 		imageHeight = image1.height;
-		
+
 		parallax = .2;
-		
+
 		this.addEventListener(Event.ENTER_FRAME, onEnterFrame);
 
 	}
